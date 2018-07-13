11f1b71fec3d3ac2bbc8f6c25f0ee52912986259
jovanka <jovanka.placeres@gmail.com>
2018-07-13 03:40:18 +0000

    added public folder w/ index.html file w/ basic setup + form, main.js update-- adding middleware to serve up static file/html and commenting out initial request w/basic text

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req,res) => {
-	res.end('we now have a res');
-})
+// app.get('/', (req,res) => {
+// 	res.end('we now have a res');
+// });
+
+// middleware
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.listen(3000, function() {
 	console.log('app is listening...');
